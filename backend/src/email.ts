9dfb0266f39fcc51758a4783594d78415aa2aa51
FranGuam <frankguansy@icloud.com>
2024-08-04 18:22:52 +0800

    feat(04): feedback email service

--- backend/src/email.ts
+++ backend/src/email.ts
@@ -1,9 +1,47 @@
 import express from "express";
+import nodemailer from "nodemailer";
 
 const router = express.Router();
 
-router.post("/contact-us", (req, res) => {
-  res.status(200).send("You are posting to /email/contact-us");
+const sendEmail = async (to: string, subject: string, text: string) => {
+  const transporter = nodemailer.createTransport({
+    host: process.env.EMAIL_HOST!,
+    port: Number(process.env.EMAIL_PORT!),
+    secure: process.env.EMAIL_SECURE! === "true",
+    auth: {
+      user: process.env.EMAIL_ADDRESS!,
+      pass: process.env.EMAIL_PASSWORD!,
+    },
+    tls: { rejectUnauthorized: false },
+  });
+  try {
+    await transporter.verify();
+    return await transporter.sendMail({ from: process.env.EMAIL_ADDRESS!, to, subject, text });
+  } catch (err) {
+    throw err;
+  }
+}
+
+router.post("/contact-us", async (req, res) => {
+  const { email, name, message } = req.body;
+  if (!email || !name || !message) {
+    return res.status(422).send("Missing email, name, or message");
+  }
+  try {
+    const result = await sendEmail(
+      process.env.EMAIL_ADDRESS!,
+      "Web Workshop Contact Us Form",
+      `Message from ${name} <${email}>:\n\n${message}`,
+    );
+    if (result.accepted.length > 0) {
+      res.status(200).send("Message sent successfully");
+    } else {
+      throw new Error("Failed to send message for unknown reason");
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Failed to send message");
+  }
 });
 
 export default router;
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import morgan from "morgan";
+import dotenv from "dotenv";
+import path from "path";
 import userRouter from "./user";
 import fileRouter from "./file";
 import emailRouter from "./email";
@@ -8,8 +10,20 @@ const app = express();
 const address = "http://localhost";
 const port = 8888;
 
+dotenv.config({
+  path: path.resolve(process.cwd(), ".local.env"),
+});
+
 // Log all requests to the console, optional.
 app.use(morgan(process.env.NODE_ENV === "production" ? "combined" : "dev"));
+
+app.all("*", (req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "*");
+  next();
+});
+
+app.use(express.json());
 
 app.use("/user", userRouter);
 app.use("/file", fileRouter);
